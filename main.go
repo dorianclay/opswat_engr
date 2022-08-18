7ba8180d93df08685c6629e2d1df1245e9334bab
Dorian Clay <dorian@dorianclay.com>
2022-08-18 10:52:37 -0400

    Complete file upload request

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"crypto/md5"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"mime/multipart"
@@ -59,58 +60,95 @@ func hashLookup(content []byte) *http.Response {
 		log.Fatal("Error making http request\n", err)
 	}
 
-	println("done.")
+	log.Println("done.")
 	return res
 }
 
-func scanFile(content []byte) *http.Response {
-	println("Checking file against Metadefender Cloud:")
+func scanFile() *http.Response {
+	// Even though we already read the file, we now need to open it
+	// so it can be uploaded as a multi-part form
+	log.Println("Checking file against Metadefender Cloud:")
 	file, err := os.Open(filename)
 	defer file.Close()
 	if err != nil {
 		log.Fatal("Error opening file\n", err)
 	}
 
+	// Initialize an empty buffer
 	body := &bytes.Buffer{}
+	// Create a new multipart writer with the buffer and the file
 	writer := multipart.NewWriter(body)
-	defer writer.Close()
-	_, err = writer.CreateFormFile("file", filename)
+	partial, err := writer.CreateFormFile("file", filename)
 	if err != nil {
 		log.Fatal("Error creating multipart writer form file\n", err)
 	}
 
-	println("Uploading file to Metadefender Cloud...")
+	// Copy the file into the writer, then close
+	io.Copy(partial, file)
+	writer.Close()
 
+	// Create an HTTP post request to upload
+	log.Println("Uploading file to Metadefender Cloud...")
 	requestURL := fmt.Sprintf("%s", getPathURL("file"))
-	// fmt.Println("Got requestURL:", requestURL)
-	// bodyReader := bytes.NewReader(content)
 	req, err := http.NewRequest(http.MethodPost, requestURL, body)
 	if err != nil {
 		log.Fatal("Error generating http request\n", err)
 	}
+	// Set required headers (filename is optional for API, but recommended)
 	req.Header.Set("apikey", apiKey)
 	req.Header.Set("Content-Type", writer.FormDataContentType())
 	req.Header.Set("filename", filename)
 
+	// Create a client with limited timeout (default is infinite)
 	client := http.Client{
 		Timeout: 30 * time.Second,
 	}
 
+	// Do the request
 	res, err := client.Do(req)
 	if err != nil {
 		log.Fatal("Error making http request\n", err)
 	}
 
-	println("done.")
+	log.Println("done.")
 
+	// If the status code is 400, the request was likely incorrect...
 	if res.StatusCode == 400 {
 		resData := bodyToMap(res)
-		log.Fatalf("Bad response, status: %s, got message: %s", res.Status, resData["messages"])
-	} else if res.StatusCode == 200 {
-		println("Successfully uploaded file.")
-	} else {
+		log.Fatalf("Bad response, status: %s, got message: %s", res.Status, resData["error"])
+		// ...or if the status isn't 200, then something went completely wrong.
+	} else if res.StatusCode != 200 {
 		log.Fatalf("Got unhandled response: %s", res.Status)
 	}
+
+	// ...otherwise the status code was 200, and upload was successful
+	resData := bodyToMap(res)
+	log.Printf("Upload successful. Spot %d in queue.\n", resData["in_queue"])
+
+	log.Printf("Scanning in progress...\n")
+	// Keep fetching the result until it is complete
+	// Create the request
+	requestURL = fmt.Sprintf("%s/%s", getPathURL("file"), resData["data_id"])
+	req, err = http.NewRequest(http.MethodGet, requestURL, nil)
+	if err != nil {
+		log.Fatal("Error generating http request\n", err)
+	}
+	req.Header.Set("apikey", apiKey)
+	req.Header.Set("x-file-metadata", "0")
+
+	// Do the request until complete
+	//   Note that although a websocket could be used here so the program is not busy waiting,
+	//   we use a simple loop so to avoid global IP setup headaches
+	for {
+		res, err = client.Do(req)
+		if err != nil {
+			log.Fatal("Error making http request\n", err)
+		}
+
+		resData = bodyToMap(res)
+
+	}
+
 	return res
 }
 
@@ -121,7 +159,7 @@ func main() {
 		filename = os.Args[1]
 	}
 
-	println("Reading file:", filename)
+	log.Println("Reading file:", filename)
 	content, err := os.ReadFile(filename)
 	if err != nil {
 		log.Fatal("Error reading file\n", err)
@@ -129,12 +167,12 @@ func main() {
 
 	res := hashLookup(content)
 
-	println("Got http response:", res.Status)
+	log.Println("Got http response:", res.Status)
 	if res.StatusCode == 404 {
-		res = scanFile(content)
-		println("Got response status:", res.Status)
+		res = scanFile()
+		log.Println("Got response status:", res.Status)
 	} else if res.StatusCode == 200 {
-		println("Need to display results...")
+		log.Println("Need to display results...")
 	} else {
 		log.Fatal("Got unhandled response:", res.Status)
 	}
